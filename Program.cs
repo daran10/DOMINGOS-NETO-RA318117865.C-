ea8869c69f1acbcdd1cbc7154c4700f034507b84
daran10 <36827107+daran10@users.noreply.github.com>
2018-08-21 14:00:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("How are you?");
+            var tudo  = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.ReadKey(true);
+            Console.Write("\nPress any key to exit...");
+        }
+    }
+}
